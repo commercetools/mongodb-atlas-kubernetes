49d6eea47f725e09128d15b00ed6197c4371f023
Yazdan Mohammadi <yazdan.mohammadi@commercetools.com>
2022-07-04 12:22:50 +0200

    Add Autoscaling simulation action

--- test/e2e/actions/extra_related.go
+++ test/e2e/actions/extra_related.go
@@ -11,6 +11,7 @@ import (
 	kubecli "github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/cli/kubecli"
 	mongocli "github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/cli/mongocli"
 	"github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/model"
+	"github.com/mongodb/mongodb-atlas-kubernetes/test/e2e/utils"
 )
 
 const (
@@ -36,6 +37,47 @@ func DeleteClusterCRD(data *model.TestDataProvider) {
 			delete(annotations, customresource.ResourcePolicyAnnotation)
 			data.Resources.Clusters[0].ObjectMeta.SetAnnotations(annotations)
 			UpdateCluster(data)
+		})
+	})
+}
+
+func Autoscaling(data *model.TestDataProvider) {
+	// TODO: This is more integration test but because of ARM problem i need to run it as e2e for now
+	By("Autoscale", func() {
+		// we deployed cluster(default=M30) with Min=M30 and Max=M50 (data/atlascluster_with_autoscaling.yaml)
+		// now we change the tier of the cluster to M40 to simulate the autoscaling
+		newTier := "M40"
+		mongocli.UpdateCluster(data.Resources.ProjectID, data.Resources.Clusters[0].Spec.DeploymentSpec.Name, "--tier", newTier)
+		// Wait for updating to be done
+		EventuallyWithOffset(1,
+			func() string {
+				return kubecli.GetK8sClusterStateName(data.Resources.Namespace, data.Resources.Clusters[0].GetClusterNameResource())
+			},
+			"45m", "1m",
+		).Should(Equal("IDLE"), "Kubernetes resource: Cluster status should be IDLE")
+
+		// Change sth in cluster to trigger reconciliation loop
+		javaScriptEnabled := false
+		data.Resources.Clusters[0].Spec.ProcessArgs.JavascriptEnabled = &javaScriptEnabled
+		By("Update cluster\n", func() {
+			utils.SaveToFile(
+				data.Resources.Clusters[0].ClusterFileName(data.Resources),
+				utils.JSONToYAMLConvert(data.Resources.Clusters[0]),
+			)
+			kubecli.Apply(data.Resources.Clusters[0].ClusterFileName(data.Resources), "-n", data.Resources.Namespace)
+		})
+		// Wait for updating to be done
+		EventuallyWithOffset(1,
+			func() string {
+				return kubecli.GetK8sClusterStateName(data.Resources.Namespace, data.Resources.Clusters[0].GetClusterNameResource())
+			},
+			"45m", "1m",
+		).Should(Equal("IDLE"), "Kubernetes resource: Cluster status should be IDLE")
+
+		By("Checking the operator doesn't scale down", func() {
+			cluster := mongocli.GetClustersInfo(data.Resources.ProjectID, data.Resources.Clusters[0].Spec.DeploymentSpec.Name)
+			clusterInstanceSize := cluster.ProviderSettings.InstanceSizeName
+			Expect(clusterInstanceSize).Should(Equal(newTier), "Operator shouldn't change the instanceSize with autoscaling enabled")
 		})
 	})
 }
